acf1cb44390b062ce0fa5caace626f8d28db16b4
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-11-03 16:39:06 +0100

    Day Two Part II

--- AdventOfCode/Day Two/Program.cs
+++ AdventOfCode/Day Two/Program.cs
@@ -4,18 +4,46 @@
 string input = InputLoader.LoadInput();
 
 // Part One
-Func<string, string, int> shapeToScore = (string a, string b) =>
+Func<string, int> shapeToInt = (string s) =>
 {
-    return char.Parse(a) - char.Parse(b);
+    char c = char.Parse(s);
+    return c > 'D' ? c - 'X' : c - 'A';
 };
 
 Func<string, int>  roundScoreCal = (token) =>
 {
     var shapes = token.Split(' ');
-    int score = (shapeToScore(shapes[0], "A") + 1) % 3 == shapeToScore(shapes[1], "X") ? 6 : shapeToScore(shapes[0], "A") == shapeToScore(shapes[1], "X") ? 3 : 0;
-    return score + shapeToScore(shapes[1], "X") + 1;
+    int score = (shapeToInt(shapes[0]) + 1) % 3 == shapeToInt(shapes[1]) ? 6 : shapeToInt(shapes[0]) == shapeToInt(shapes[1]) ? 3 : 0;
+    return score + shapeToInt(shapes[1]) + 1;
 };
 
-var tokens = input.Split('\n').SkipLast(1).Select(x => roundScoreCal(x)).Sum();
+var result = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(x => roundScoreCal(x)).Sum();
 
-Console.WriteLine($"Part One answer: {tokens}");
+Console.WriteLine($"Part One answer: {result}");
+
+// Part Two
+
+Func<string, string> translate = (token) =>
+{
+    var shapes = token.Split(' ');
+    char move;
+    switch (shapes[1])
+    {
+        case "X":
+            move = (char)((shapeToInt(shapes[0]) + 2) % 3 + 'X');
+            break;
+        case "Z":
+            move = (char)((shapeToInt(shapes[0]) + 1) % 3 + 'X');
+            break;
+        default:
+            move = (char)(shapeToInt(shapes[0]) + 'X');
+            break;
+    }
+    return shapes[0] + ' ' + move;
+};
+
+result = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(x => translate(x)).Select(x => roundScoreCal(x)).Sum();
+
+var tmp = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(x => translate(x));
+
+Console.WriteLine($"Part Two answer: {result}");
