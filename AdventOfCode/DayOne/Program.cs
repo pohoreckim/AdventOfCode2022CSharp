7485a087def9f8e445123f62833ee0ca1451a0de
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-11-03 14:12:53 +0100

    Day One Comments

--- AdventOfCode/DayOne/Program.cs
+++ AdventOfCode/DayOne/Program.cs
@@ -2,15 +2,18 @@
 using System.Collections;
 using Utils;
 
+// Task input
 string input = InputLoader.LoadInput();
+
+// Part One
 List<int> sums = new List<int>();
 input.Split('\n').Select(x => { try { return Convert.ToInt32(x); } catch (Exception) { return -1; } })
     .Aggregate(0, (a, b) => { if (b != -1) return a + b; else sums.Add(a); return 0; });
-
 int maxValue = sums.Max();
 
 Console.WriteLine($"Max value: {maxValue}");
 
+// Part Two
 int threeMaxValues = sums.OrderByDescending(x => x).Take(3).Sum();  
 
 Console.WriteLine(threeMaxValues.ToString());
